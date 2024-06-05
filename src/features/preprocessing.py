f99f48c183aa01c507a17ac503d3656c3e1fb414
Ben Xiao <benjamin.y.xiao@gmail.com>
2024-06-04 21:38:41 -0700

    updating repo structure

---
+++ src/features/preprocessing.py
@@ -0,0 +1,10 @@
+"""
+This file contains all the functions required to preprocess the data before training the model.
+
+1. Convert raw video file to audio file.
+2. Use AI model to clarify audio file.
+3. Use AI speaker diarization model to separate audio file into speaker segments.
+4. Function for splicing together relevant chunks of audio files specified.
+5. Transcribe audio file to text file.
+
+"""
